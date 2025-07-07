7bb5f5ac08b52b5075d950bad86396183c35dd99
taegyun1995 <needle12s@gmail.com>
2025-07-07 20:06:40 +0900

    BFS 알고리즘: 숨바꼭질

---
+++ BFS_바킹독_과제/숨박꼭질_1697/Main_이태균.java
@@ -0,0 +1,67 @@
+package 숨박꼭질_1697;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_이태균 {
+
+    private static int N;
+    private static int K;
+    private static boolean[] VISITED = new boolean[100001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs());
+    }
+
+    private static int bfs() {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(N, 0));
+        VISITED[N] = true;
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            int now_pos = node.pos;
+            int now_time = node.time;
+
+            if (now_pos == K) {
+                return now_time;
+            }
+
+            if (now_pos * 2 <= 100000 && !VISITED[now_pos * 2]) {
+                queue.add(new Node(now_pos * 2, now_time + 1));
+                VISITED[now_pos * 2] = true;
+            }
+            if (now_pos - 1 <= 0 && !VISITED[now_pos - 1]) {
+                queue.add(new Node(now_pos - 1, now_time + 1));
+                VISITED[now_pos - 1] = true;
+            }
+            if (now_pos + 1 <= 100000 && !VISITED[now_pos + 1]) {
+                queue.add(new Node(now_pos + 1, now_time + 1));
+                VISITED[now_pos + 1] = true;
+            }
+        }
+
+        return -1;
+    }
+
+    static class Node {
+        int pos;
+        int time;
+
+        public Node(int pos, int time) {
+            this.pos = pos;
+            this.time = time;
+        }
+    }
+
+}
