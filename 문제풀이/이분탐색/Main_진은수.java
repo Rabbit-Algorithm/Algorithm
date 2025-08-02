cf5453310cbb92df83852ddc0e766ea5c29e3a0f
ensu <ensu6788@gmail.com>
2025-08-02 12:01:43 +0900

    이분탐색

---
+++ 문제풀이/이분탐색/Main_진은수.java
@@ -0,0 +1,24 @@
+package 문제풀이.이분탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_진은수 {
+
+    /**
+     * 차집합
+     * https://www.acmicpc.net/problem/1822
+     * 실버1
+     */
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+
+
+}
