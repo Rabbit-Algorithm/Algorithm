c0a1b59fd4de34272c02b7fbae6fb4d81f62ef85
HongChangMo <hcm7402@naver.com>
2025-08-10 20:06:56 +0900

    update

--- 문제풀이/이분탐색/숫자카드2_10816/Main_홍창모.java
+++ 문제풀이/이분탐색/숫자카드2_10816/Main_홍창모.java
@@ -1,2 +1,81 @@
-package 문제풀이.이분탐색.숫자카드2_10816;public class Main_홍창모 {
+package 문제풀이.이분탐색.숫자카드2_10816;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_홍창모 {
+
+    static int N, M;
+    static int[] ARR1, ARR2;
+    static StringBuilder SB;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        ARR1 = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for( int i = 0; i < N; i++ ) {
+            ARR1[i] = Integer.parseInt(st.nextToken());
+        }
+
+        M = Integer.parseInt(br.readLine());
+        ARR2 = new int[M];
+        st = new StringTokenizer(br.readLine());
+
+        for( int i = 0; i < M; i++ ) {
+            ARR2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(ARR1);
+        SB = new StringBuilder();
+
+        for( int i = 0; i < M; i++ ) {
+            int cnt = upperBound(ARR2[i]) - lowerBound(ARR2[i]);
+            SB.append(cnt).append(" ");
+        }
+
+        System.out.println(SB);
+    }
+
+    public static int lowerBound(int target) {
+        // target이 시작하는 가장 왼쪽 인덱스를 찾는다.
+        int lt = 0;
+        int rt = N;
+
+        while (lt < rt) {
+            int mid = lt + (rt - lt) / 2;
+
+            if (ARR1[mid] >= target) {
+                rt = mid;
+            } else {
+                lt = mid + 1;
+            }
+        }
+
+        return lt;
+    }
+
+    public static int upperBound(int target) {
+
+        // target이 끝나는 가장 오른쪽 인덱스를 찾는다.
+
+        int lt = 0;
+        int rt = N;
+
+        while (lt < rt) {
+            int mid = lt + (rt - lt) / 2;
+
+            if (ARR1[mid] > target) {
+                rt = mid;
+            } else {
+                lt = mid + 1;
+            }
+        }
+
+        return lt;
+    }
 }
--- 문제풀이/이분탐색/숫자카드_10815/Main_홍창모.java
+++ 문제풀이/이분탐색/숫자카드_10815/Main_홍창모.java
@@ -1,2 +1,70 @@
-package 문제풀이.이분탐색.숫자카드_10815;public class Main_홍창모 {
+package 문제풀이.이분탐색.숫자카드_10815;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_홍창모 {
+
+    static int N, M;
+    static int[] ARR1, ARR2;
+    static StringBuilder SB;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        ARR1 = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for( int i = 0; i < N; i++ ) {
+            ARR1[i] = Integer.parseInt(st.nextToken());
+        }
+
+        M = Integer.parseInt(br.readLine());
+        ARR2 = new int[M];
+        st = new StringTokenizer(br.readLine());
+
+        for( int i = 0; i < M; i++ ) {
+            ARR2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 정렬 처리
+        Arrays.sort(ARR1);
+
+        SB = new StringBuilder();
+
+        binarySearch();
+
+        System.out.println(SB);
+    }
+
+    private static void binarySearch() {
+
+        for( int i = 0; i < M; i++ ) {
+            int lt = 0;
+            int rt = N-1;
+
+            boolean found = false;
+            while (lt <= rt) {
+                int mid = lt + (rt-lt) / 2;
+
+                if( ARR1[mid] < ARR2[i] ) {
+                    lt = mid + 1;
+                } else if( ARR1[mid] == ARR2[i] ) {
+                    found = true;
+                    break;
+                } else {
+                    rt = mid - 1;
+                }
+            }
+
+            if( found ) {
+                SB.append(1).append(" ");
+            } else {
+                SB.append(0).append(" ");
+            }
+        }
+    }
 }
