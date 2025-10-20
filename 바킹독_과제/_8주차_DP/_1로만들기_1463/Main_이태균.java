d4d620d5567c0970ff12dbc597f21d1c3082b898
taegyun <needle12s@gmail.com>
2025-10-20 12:56:57 +0900

    다이나믹 프로그래밍: 1로 만들기

---
+++ 바킹독_과제/_8주차_DP/_1로만들기_1463/Main_이태균.java
@@ -0,0 +1,35 @@
+package _8주차_DP._1로만들기_1463;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_이태균 {
+
+    static int   N;
+    static int[] DP;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        DP = new int[N + 1];
+        DP[0] = 0;
+        DP[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            DP[i] = DP[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+            }
+        }
+
+        System.out.println(DP[N]);
+    }
+
+}
